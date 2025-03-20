c76283b68aafbb72624013608992c62dab86c8ca
reedse_7033251 <111336292+reedse@users.noreply.github.com>
2025-03-20 13:17:59 -0400

    vercel analytics
    -installed and added vercel analytics package to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Metadata } from "next"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -123,6 +124,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
